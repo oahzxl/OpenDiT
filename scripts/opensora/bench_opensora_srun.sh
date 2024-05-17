6a04ffb494ae3da22aea1af046a0d75c574bd9e4
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-05-17 02:46:50 -0700

    update script

--- scripts/opensora/bench_opensora_srun.sh
+++ scripts/opensora/bench_opensora_srun.sh
@@ -8,13 +8,14 @@ H=$8
 W=$9
 SP=${10}
 SP_SIZE=${11}
+MODEL_TYPE=${12}
 
 source bash_slurm_env.sh
 
 mkdir -p log
 
-echo "run WARMUP=$WARMUP RUNTIME=$RUNTIME BATCH_SIZE=$BATCH_SIZE NUM_FRAMES=$NUM_FRAMES H=$H W=$W SP=$SP SP_SIZE=$SP_SIZE"
-srun -N${NUM_NODES} --gres=gpu:${GRES} --ntasks-per-node=1 --cpus-per-task=${CPUS} --job-name=$3 --mem=0 \
+echo "run WARMUP=$WARMUP RUNTIME=$RUNTIME BATCH_SIZE=$BATCH_SIZE NUM_FRAMES=$NUM_FRAMES H=$H W=$W SP=$SP SP_SIZE=$SP_SIZE MODEL_TYPE=$MODEL_TYPE"
+srun -N${NUM_NODES} --gres=gpu:${GRES} --ntasks-per-node=1 --cpus-per-task=${CPUS} -t 00:10:00 --job-name=$3 --mem=0 \
 python $slurm_launcher --script scripts/opensora/bench_opensora.py --main_process_port 34089 \
     --batch_size $BATCH_SIZE \
     --mixed_precision bf16 \
@@ -29,4 +30,5 @@ python $slurm_launcher --script scripts/opensora/bench_opensora.py --main_proces
     --image_size $H $W \
     --sp $SP \
     --sequence_parallel_size $SP_SIZE \
+    --model_type $MODEL_TYPE \
     > >(tee log/batch${BATCH_SIZE}_f${NUM_FRAMES}_h${H}_w${W}_sp${SP_SIZE}_${SP}.log) 2> >(tee log/batch${BATCH_SIZE}_f${NUM_FRAMES}_h${H}_w${W}_sp${SP_SIZE}_${SP}-error.log >&2)
