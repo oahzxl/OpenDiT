86869cdbccd2cde22a7b7995ec567964a145c94b
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-05-20 12:36:58 -0700

    Update benchmark script for weak scaling with larger model size and shorter warmup/runtime

--- bench_weak_scaling_l_inter.sh
+++ bench_weak_scaling_l_inter.sh
@@ -3,13 +3,13 @@ RUNTIME=1
 MODEL_TYPE="3B"
 
 HYPE_LIST=(
-    "(8 128 1024 1024 64)"
-    "(4 128 1024 1024 32)"
     "(2 128 1024 1024 16)"
     "(1 128 1024 1024 8)"
+    # "(8 128 1024 1024 64)"
+    "(4 128 1024 1024 32)"
 )
 
-sp_list=("dsp" "megatron" "ulysses")
+sp_list=("ring")
 
 for hype in "${HYPE_LIST[@]}"
 do
