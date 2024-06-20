ab7af26c3940016b504ef13997bbcaa9e910addf
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-06-20 17:41:58 +0000

    update license

--- opendit/models/opensora/stdit3.py
+++ opendit/models/opensora/stdit3.py
@@ -1,3 +1,13 @@
+# Adapted from OpenSora
+
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+# --------------------------------------------------------
+# References:
+# OpenSora: https://github.com/hpcaitech/Open-Sora
+# --------------------------------------------------------
+
+
 import os
 
 import numpy as np
