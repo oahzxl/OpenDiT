fff2617175b62eb25834f8e04c715da2a492c1a2
Jxl <jinxiaolong1129@gmail.com>
2024-06-23 03:59:06 -0400

    Eval (#18)
    * finish opensora-plan and latte
    
    * finish opensora
    
    * code refine
    
    * code refine
    
    * final code clean
    
    * update skip_diffusion_timestep function to handle more than 1 step skip
    
    * update code
    
    * polish code
    
    * polish print
    
    * update default value
    
    * update verbose
    
    * finish latte eval generate function
    
    * update gitignore
    
    * finish eval
    TODO:
    BUG check whether center crop is correct
    TODO original video size< generated videos, how to resize?
    
    * update eval save
    
    ---------
    
    Co-authored-by: Jinxiaolong1129 <inxiaolong1129@gmail.com>
    Co-authored-by: Xuanlei Zhao <xuanlei.zhao@gmail.com>

---
+++ evaluations/fastvideodiffusion/scripts/latte/generate_eval_latte_dataset.sh
@@ -0,0 +1,3 @@
+torchrun --standalone --nproc_per_node=1 evaluations/fastvideodiffusion/scripts/latte/eval_latte.sh --config evaluations/fastvideodiffusion/configs/latte/sample.yaml
+
+# torchrun --standalone --nproc_per_node=8 evaluations/fastvideodiffusion/scripts/latte/eval_latte.sh --config evaluations/fastvideodiffusion/configs/latte/sample_skip.yaml
