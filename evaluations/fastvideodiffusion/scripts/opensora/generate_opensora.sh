1554af5c2d1247633fbbcc43333be136a44ced2f
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-06-24 04:53:16 +0000

    update generate

--- evaluations/fastvideodiffusion/scripts/opensora/generate_opensora.sh
+++ evaluations/fastvideodiffusion/scripts/opensora/generate_opensora.sh
@@ -1,4 +1,5 @@
-torchrun --standalone --nproc_per_node=1 evaluations/fastvideodiffusion/scripts/opensora/sample_opensora.py --config evaluations/fastvideodiffusion/configs/opensora/sample.yaml
+# origin
+torchrun --standalone --nproc_per_node=8 evaluations/fastvideodiffusion/scripts/opensora/generate_opensora.py --config evaluations/fastvideodiffusion/configs/opensora/sample.yaml
 
-# skip
-# torchrun --standalone --nproc_per_node=8 evaluations/fastvideodiffusion/scripts/opensora/sample_opensora.py --config evaluations/fastvideodiffusion/configs/opensora/sample_skip.yaml
+# fvd
+torchrun --standalone --nproc_per_node=8 evaluations/fastvideodiffusion/scripts/opensora/generate_opensora.py --config evaluations/fastvideodiffusion/configs/opensora/sample_fvd.yaml
