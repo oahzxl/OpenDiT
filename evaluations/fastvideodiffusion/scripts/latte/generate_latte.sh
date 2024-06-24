bc8f8fc1e9eb8278bd99cd289b36ebd2fa741a1b
Xuanlei Zhao <xuanlei.zhao@gmail.com>
2024-06-24 03:52:28 +0000

    update eval

---
+++ evaluations/fastvideodiffusion/scripts/latte/generate_latte.sh
@@ -0,0 +1,5 @@
+# origin
+torchrun --standalone --nproc_per_node=8 evaluations/fastvideodiffusion/scripts/latte/generate_latte.py --config evaluations/fastvideodiffusion/configs/latte/sample.yaml
+
+# fvd
+# torchrun --standalone --nproc_per_node=1 evaluations/fastvideodiffusion/scripts/latte/eval_latte.sh --config evaluations/fastvideodiffusion/configs/latte/sample_skip.yaml
